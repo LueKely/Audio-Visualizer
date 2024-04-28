5bdb0a8fd7efbbe582884989ce485ef2ea46c5f8
LueKely <luekely43110@gmail.com>
2024-04-28 14:20:22 +0800

    poop

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -15,8 +15,8 @@ vec4 pos = texture2D(uPositions, uv);
 float angle = atan(pos.y,pos.x);
 
 
-vec3 colorA = vec3(0.07, 0.46, 0.91);
-vec3 colorB = vec3(0.91, 0.12, 0.24);
+vec3 colorA = vec3(0.75, 0.94, 0.07);
+vec3 colorB = vec3(0.07, 0.93, 0.25);
 float pct = uFreq;
 
 vec3 mixed = mix(colorA, colorB, pct);
